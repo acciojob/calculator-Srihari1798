d4147ec539a99260a5e47f4c2469011ace1347c8
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-02-17 14:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+var buttons = document.getElementByTagName('buttons');
+var input = document.getElementById('input');
+ for(var i=0;i<buttons.length;i++){
+	 buttons[i].onclick=function();{
+	 var result = this.innerText;
+	 updateDisplay(result);
+	 }
+ }
+function updateDisplay(result){
+	var currentDisplay =input.innerText
+	if(currentDisplay=="0"){
+		
+	}
+}
